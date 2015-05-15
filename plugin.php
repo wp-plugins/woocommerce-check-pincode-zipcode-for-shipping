ac070f70cbd99593c6b4d9081f8802e86dc3eaa5
phoeniixx <phoeniixx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 11:21:25 +0000

    Tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-check-pincode-zipcode-for-shipping/trunk@1161089 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Woocommerce check pincode/zipcode for shipping
 Plugin URI: http://www.phoeniixx.com
 Description: Woocommerce Product Pincode Check.
-Version: 1.0
+Version: 1.1
 Author: Phoeniixx Team
 Author URI: http://www.phoeniixx.com
 */
