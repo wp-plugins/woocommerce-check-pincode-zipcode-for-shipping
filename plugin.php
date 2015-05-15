1459f67958a38d572183bd15124208006be43a53
phoeniixx <phoeniixx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 12:46:51 +0000

    changes
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-check-pincode-zipcode-for-shipping/trunk@1161123 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -14,8 +14,8 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 /**
 
  * Check if WooCommerce is active
-
- **/
+ 
+**/
 
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) 
 {
