92d2239144ab0386b86bf8447226cd7233019915
phoeniixx <phoeniixx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-16 11:43:22 +0000

    Description change
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-check-pincode-zipcode-for-shipping/trunk@1181821 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Woocommerce check pincode/zipcode for shipping
 Plugin URI: http://www.phoeniixx.com
-Description: Woocommerce Product Pincode Check.
+Description: Advance Check Pin Code is a solution that allows users to set delivery dates based on the pin codes.
 Version: 1.1
 Author: Phoeniixx Team
 Author URI: http://www.phoeniixx.com
