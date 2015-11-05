afa4700fea852edc1ae95e42cb2184dd6cfab8fe
phoeniixx <phoeniixx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-05 12:34:50 +0000

    new version release
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-check-pincode-zipcode-for-shipping/tags/1.3@1280338 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,112 @@
+=== Woocommerce check pincode/zipcode for shipping ===
+Contributors: phoeniixx
+Tags: phoeniixx,pincode, zipcode, woocommerce, e-commerce, shop, cod, dod, cash on delivery ,Woocommerce check pincode,Woocommerce check pincode/zipcode for shipping 
+Requires at least: 4.0
+Tested up to: 4.3.1
+Stable tag: 1.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Advance Check Pin Code is a solution that allows users to set delivery dates based on the pin codes.
+
+== Description ==
+
+**Advance checking of pin codes** is a great feature. Online shoppers usually like knowing the timeline of their orders before they place an order. With this plugin you can tell them about the delivery dates of your products before they move towards the buying stage. This will reduce your cart abandonment rates and increase your customer satisfaction rates.
+
+**Advance Check Pin Code Features:**
+
+1. Can display the delivery dates on product pages only
+2. Can Enable/Disable Help Text for Delivery date option
+3. Can add Help Text for Delivery date option
+4. Customers can check pin codes for shipping on product page
+5. Delivery dates are shown based on the pin codes
+6. Basic styling of Background color, Text color and Button color
+
+**Premium features**
+
+1. Can upload the pin codes sheet form the backend( only in CSV format)
+2. CSV sheet format is - Pincode: City: State: Delivery within days: Cash on delivery
+3. Can Enable/Disable Help Text for Delivery date and Cash on delivery option
+4. Can have COD verification based on pin codes
+5. Can display the delivery dates on product page, Cart page and Checkout page
+6. Can add Help Text for COD and Delivery date options
+7. Can keep the delivery service off for Saturdays and Sundays
+8. Customers can check pin codes for shipping on product and checkout page
+9. Delivery dates are shown based on the pin codes
+10. Advance Styling options
+11. Uploading Image options.
+12. Option to hide or show Pincode Area on Product Page
+13. Choice to validate or not validate Pincode, on Product Page
+14. Option to pick Pincode from default billing/shipping address of customer
+15. Option to hide or show "Cash on Delivery"  and/or "Delivery Days/Date" on Product Page
+16. You can also use this shortcode "[phoeniixx-pincode-check]" to add pincode area on any page.
+
+
+**Premium Version link** - [Click Here](http://www.phoeniixx.com/product/woocommerce-check-pincodezipcode-for-shipping-and-cod/?utm_source=Wordpress.org&utm_medium=CPC&utm_campaign=Wordpress.org)
+
+**Support**
+
+**Email :** - [tarun@phoeniixx.com](mailto:tarun@phoeniixx.com)
+
+**Skype** - phoeniixx.tarun
+
+**Ticket system** - [Click here](http://www.phoeniixx.com/support)
+
+**Website** - [http://www.phoeniixx.com/products/](http://www.phoeniixx.com/products/)
+
+For a more detailed list of options and features of the plugin, please look at the [Official Documentation](http://www.phoeniixx.com/docs/woocommerce/woocommerce-check-pincode/index.html)
+
+== Installation ==
+
+1. Install [WordPress](https://wordpress.org/)
+2. Go to your WordPress admin panel, and then to Plugins. Click on "Add New" and then upload the zip file of the plugin using the "Upload Plugin" button you can find on top of the screen.
+3. For the plugin to work as it should, WooCommerce plugin has to be installed and enabled.
+
+== Frequently Asked Questions ==
+
+* How can I upgrade from a free version to a premium?
+
+**Ans.** [Visit this page](http://www.phoeniixx.com/product/woocommerce-check-pincodezipcode-for-shipping-and-cod/), purchase the plugin from there and install it into your theme. When you activate the premium version, the free version automatically gets deactivated.
+
+* Will I lose the settings of the free version if I switch to the Premium Version?
+
+**Ans.** No, the premium version offers a list of additional features that will work in addition to the features of the free version.
+
+* Do I get access to different styling options for the Check Pincode Functionality on the Product Page?
+
+**Ans.** Yes, the premium feature offers a variety of styling options that are available in the "Styling Options" feature that is present under the Settings Tab.
+
+
+* Is there an option to update Pin codes?
+
+**Ans.** Yes, there is an option to update Pin codes in our plugin.
+
+* How many Pin codes can I add?
+
+**Ans.** You can add an unlimited number of pin codes.
+
+
+* Is there any documentation available for this plugin?
+
+**Ans.** Yes, you can get complete access to the documentation of this plugin at this [Link](http://www.phoeniixx.com/docs/woocommerce/woocommerce-check-pincode/index.html)
+
+== Screenshots ==
+
+1. This page shows how the pincode settings applied on your site looks.
+2. This page shows all the settings. If you want to change some settings regarding pincode, it can be done on this page.
+3. This page is used to add Zipcodes and can set the delivery date.  
+
+
+== Changelog ==
+
+= 1.3 - 05/11/2015 =
+* Bug Fixes.
+
+= 1.2 - 14/10/2015 =
+* Removed the limit of pincode length .
+
+= 1.1 - 21/07/2015 =
+* Fixed warning messages.
+
+= 1.0 =
+* Initial release.
---
+++ premium-setting.php
@@ -0,0 +1,183 @@
+<?php
+$plugin_dir_url =  plugin_dir_url( __FILE__ );
+?>
+<style>
+.premium-box{ width:100%; height:auto; background:#fff;  }
+.premium-features{}
+.premium-heading{  color: #484747;font-size: 40px;padding-top: 35px;text-align: center;text-transform: uppercase;}
+.premium-features li{ width:100%; float:left;  padding: 80px 0; margin: 0; }
+.premium-features li .detail{ width:50%; }
+.premium-features li .img-box{ width:50%; }
+
+.premium-features li:nth-child(odd) { background:#f4f4f9; }
+.premium-features li:nth-child(odd) .detail{float:right; text-align:left; }
+.premium-features li:nth-child(odd) .detail .inner-detail{}
+.premium-features li:nth-child(odd) .detail p{ }
+.premium-features li:nth-child(odd) .img-box{ float:left; text-align:right;}
+
+.premium-features li:nth-child(even){  }
+.premium-features li:nth-child(even) .detail{ float:left; text-align:right;}
+.premium-features li:nth-child(even) .detail .inner-detail{ margin-right: 46px;}
+.premium-features li:nth-child(even) .detail p{ float:right;} 
+.premium-features li:nth-child(even) .img-box{ float:right;}
+
+.premium-features .detail{}
+.premium-features .detail h2{ color: #484747;  font-size: 24px; font-weight: 700; padding: 0;}
+.premium-features .detail p{  color: #484747;  font-size: 13px;  max-width: 327px;}
+/**images**/
+.pincode-check{ background:url(<?php echo $plugin_dir_url; ?>assets/img/change-img.jpg); width:507px; height:248px; display:inline-block; margin-right: 25px; background-repeat:no-repeat;}
+
+.sat-sun-off{ background:url(<?php echo $plugin_dir_url; ?>assets/img/saturday-sunday-off.jpg); width:500px; height:161px; display:inline-block; background-size:500px auto; margin-right:30px; background-repeat:no-repeat;}
+
+.bulk-upload{ background:url(<?php echo $plugin_dir_url; ?>assets/img/bulk-csv-uploads.jpg); width:364px; height:275px; display:inline-block; background-repeat:no-repeat;}
+
+.cod-verify{background:url(<?php echo $plugin_dir_url; ?>assets/img/cod-check.jpg); width:469px; height:167px; display:inline-block; margin-right:30px; background-repeat:no-repeat;}
+
+.delivery-date{background:url(<?php echo $plugin_dir_url; ?>assets/img/delivery-date.jpg); width:490px; height:199px; display:inline-block; background-repeat:no-repeat;}
+
+.advance-styling{background:url(<?php echo $plugin_dir_url; ?>assets/img/style-check.jpg); width:440px; height:335px; display:inline-block; margin-right:30px; background-repeat:no-repeat;}
+
+.Checkout-Page-Pincode--Check{background:url(<?php echo $plugin_dir_url; ?>assets/img/Checkout-Page-Pincode-Check.jpg); width:488px; height:260px; display:inline-block; background-repeat:no-repeat;}
+
+/*upgrade css*/
+
+.upgrade{background:#f4f4f9;padding: 50px 0; width:100%; clear: both;}
+.upgrade .upgrade-box{ background-color: #808a97;
+    color: #fff;
+    margin: 0 auto;
+   min-height: 110px;
+    position: relative;
+    width: 60%;}
+
+.upgrade .upgrade-box p{ font-size: 15px;
+     padding: 19px 20px;
+    text-align: center;}
+
+.upgrade .upgrade-box a{background: none repeat scroll 0 0 #6cab3d;
+    border-color: #ff643f;
+    color: #fff;
+    display: inline-block;
+    font-size: 17px;
+    left: 50%;
+    margin-left: -150px;
+    outline: medium none;
+    padding: 11px 6px;
+    position: absolute;
+    text-align: center;
+    text-decoration: none;
+    top: 36%;
+    width: 277px;}
+
+.upgrade .upgrade-box a:hover{background: none repeat scroll 0 0 #72b93c;}
+
+.premium-vr{ text-transform:capitalize;} 
+
+</style>
+
+<div class="premium-box">
+
+<div class="upgrade">
+<div class="upgrade-box">
+<!--<p> Switch to the premium version of Woocommerce Check Pincode/Zipcode for Shipping and COD to get the benefit of all features! </p>
+--><a target="_blank" href="http://www.phoeniixx.com/product/woocommerce-check-pincodezipcode-for-shipping-and-cod/"><b>UPGRADE</b> to the <span class="premium-vr">premium version</span></a>
+
+</div>
+</div>
+
+<ul class="premium-features">
+<h1 class="premium-heading">Premium Features</h1>
+<li>
+
+<div class="img-box"><span class="pincode-check"></span></div>
+ <div class="detail">
+ <div class="inner-detail">
+   <h2>Add to Cart Activation based on Pincodes</h2>
+    <p>
+       The Add to Cart Button won&acute;t work unless the Pincode that the user has entered is available for delivery as per your list of available pin codes.
+    </p>
+  </div> 
+</li>
+
+<li>
+ <div class="detail">
+  <div class="inner-detail">
+   <h2>Saturday/Sunday Off Option</h2>
+    <p>
+      You have the option to activate or deactivate the Saturday/Sunday off option to ensure that no delivery dates are set for Saturdays or Sundays.
+    </p>
+   </div> 
+ </div>
+ <div class="img-box"><span class="sat-sun-off"></span></div>
+</li>
+
+<li>
+<div class="img-box"><span class="bulk-upload"></span></div>
+
+ <div class="detail">
+ <div class="inner-detail">
+   <h2>Bulk CSV Uploads</h2>
+    <p>
+       With bulk CSV uploads option you can enter as many pin codes as you want into the system in one go. There will be no need to manually add each Pin Code.
+    </p>
+  </div> 
+</li>
+
+<li>
+ <div class="detail">
+  <div class="inner-detail">
+   <h2>COD Verification</h2>
+    <p>
+      You have the option to choose which pin codes will support the  COD option and which pin codes won&acute;t.
+    </p>
+   </div> 
+ </div>
+ <div class="img-box"><span class="cod-verify"></span></div>
+</li>
+
+<li>
+
+<div class="img-box"><span class="delivery-date"></span></div>
+
+ <div class="detail">
+ <div class="inner-detail">
+   <h2>Delivery Dates Preview</h2>
+    <p>
+       The delivery date for the pincodes chosen by the user will appear on the Product Page, Cart Page and even the Checkout Page.
+    </p>
+  </div> 
+  </div>
+</li>
+
+<li>
+ <div class="detail">
+  <div class="inner-detail">
+   <h2>Advanced Styling Options</h2>
+    <p>
+     You will get access to many advanced styling options to create a Check Pin Code format that will gel completely with your website.
+    </p>
+   </div> 
+ </div>
+ <div class="img-box"><span class="advance-styling"></span></div>
+</li>
+
+<li>
+ <div class="detail">
+  <div class="inner-detail">
+   <h2>Checkout Page Pincode Check</h2>
+    <p>
+      Usually customers enter a different pincode on Checkout page and process their order. But with this feature the Customer cannot enter a different Pincode in the checkout step of the order process as the Pincodes will be checked on Checkout Page also
+    </p>
+   </div> 
+ </div>
+ <div class="img-box"><span class="Checkout-Page-Pincode--Check"></span></div>
+</li>
+</ul>
+
+<div class="upgrade">
+<div class="upgrade-box">
+<!--<p> Switch to the premium version of Woocommerce Check Pincode/Zipcode for Shipping and COD to get the benefit of all features! </p>
+--><a target="_blank" href="http://www.phoeniixx.com/product/woocommerce-check-pincodezipcode-for-shipping-and-cod/"><b>UPGRADE</b> to the <span class="premium-vr">premium version</span></a>
+</div>
+
+</div>
+</div>
